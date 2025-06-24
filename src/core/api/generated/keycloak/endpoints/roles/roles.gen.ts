6865faa53bfbe14af32520c4c216e77951968924
Syed Usman <syedus06@gmail.com>
2025-06-24 17:43:15 +0500

    Rename multiple page and layout files for consistency across components

--- src/core/api/generated/keycloak/endpoints/roles/roles.gen.ts
+++ src/core/api/generated/keycloak/endpoints/roles/roles.gen.ts
@@ -146,10 +146,10 @@ export const getAdminRealmsRealmClientsClientUuidRolesRoleNameCompositesClientsC
     realm: string,
     clientUuid: string,
     roleName: string,
-    clientUuid: string,
+    clientUuidt: string,
  options?: SecondParameter<typeof keycloakServiceMutator>,) => {
       return keycloakServiceMutator<RoleRepresentation[]>(
-      {url: `/admin/realms/${realm}/clients/${clientUuid}/roles/${roleName}/composites/clients/${clientUuid}`, method: 'GET'
+      {url: `/admin/realms/${realm}/clients/${clientUuid}/roles/${roleName}/composites/clients/${clientUuidt}`, method: 'GET'
     },
       options);
     }
