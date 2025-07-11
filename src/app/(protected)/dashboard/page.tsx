be4fdb754683f3f7ed17f0bfdc77f92be4027e06
Syed Usman <syedus06@gmail.com>
2025-07-11 23:17:43 +0500

    Add role-based access control to DashboardPage and update sidebar item

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,5 +1,19 @@
+"use client";
+
 import { DashboardOverview } from '@/features/dashboard/components/DashboardOverview';
+import {useUserAuthorities} from "@/core/auth";
+import {useRouter} from "next/navigation";
+
 
 export default function DashboardPage() {
+  const router = useRouter();
+
+  const { hasRole } = useUserAuthorities();
+
+  if (!hasRole('dashboard')) {
+    router.push('/calls');
+    return null ;
+  }
+
   return <DashboardOverview />;
 }
