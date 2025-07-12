3cc951d81c79728029bfba50c871023d2e595349
Syed Usman <syedus06@gmail.com>
2025-07-12 22:49:09 +0500

    Refactor DashboardPage to remove role-based redirection logic

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,18 +1,7 @@
 "use client";
 
 import {DashboardOverview} from '@/features/dashboard/components/DashboardOverview';
-import {useUserAuthorities, useUserRoles} from "@/core/auth";
-import {useRouter} from "next/navigation";
-
 
 export default function DashboardPage() {
-    const router = useRouter();
-    const {roles: userRoles, isLoading: rolesLoading} = useUserRoles();
-
-    if (!userRoles.includes('dashboard')) {
-        router.push('/calls');
-        return null;
-    }
-
     return <DashboardOverview/>;
 }
