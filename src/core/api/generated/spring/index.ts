f1e78089f9f31ec2b645bbbce132c5e58b4bdcf1
Abdul Rehman <abdulrehmantahir353@gmail.com>
2025-11-01 16:54:56 +0500

    add the new column "issue" in the importHistory

--- src/core/api/generated/spring/index.ts
+++ src/core/api/generated/spring/index.ts
@@ -46,3 +46,4 @@ export * from './endpoints/call-type-bulk-import-controller/call-type-bulk-impor
 export * from './endpoints/call-status-bulk-import-controller/call-status-bulk-import-controller.gen';
 export * from './endpoints/product-sub-category-resource/product-sub-category-resource.gen';
 export * from './endpoints/product-category-resource/product-category-resource.gen';
+export * from './endpoints/import-history-resource/import-history-resource.gen';
